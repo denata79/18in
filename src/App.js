2af52a03b1f3b594c9614fb72851aec4585d4bca
denata79 <denata79@yandex.ru>
2024-06-05 23:29:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
+import Swal from 'sweetalert2';
 import './App.css';
+import { useTranslation } from 'react-i18next';
+
 
 function App() {
+
+const handelAlert = () => {
+  Swal.fire({
+    position: "top-center",
+    icon: "success",
+    title: "Запрос отправлен",
+    showConfirmButton: false,
+    timer: 2500
+  });
+}
+const { t, i18n } = useTranslation();
+
+const toggleLang = () => {
+  i18n.changeLanguage(i18n.language === 'en' ? "ru" : 'en')
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handelAlert}> Alert</button>
+      <h1> {t ("Welcome to React")}</h1>
+      <button onClick={toggleLang}> 
+        {i18n.language === 'en' ? "en" : 'ru'}
+      </button>
     </div>
   );
 }
---
+++ src/i18n.js
@@ -0,0 +1,35 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)fit 
+  .init({
+    debug: true,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    // resources: {
+    //     en: {
+    //       translation: {
+             
+    //           "Welcome to React" : "Welcome to React"
+            
+    //       }
+    //     },
+    //     ru: {
+    //       translation: {
+             
+    //             "Welcome to React" :"учи реакт"
+            
+    //       }
+    //     }
+    //   }
+    });
+
+
+
+
+export default i18n;
